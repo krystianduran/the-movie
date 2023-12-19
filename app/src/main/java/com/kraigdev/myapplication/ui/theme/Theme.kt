f5b2638490d8c28f4ce3591f66db78f555fed564
Krystian Durán <krystian.duran@gmail.com>
2023-12-19 18:43:17 -0500

    add format, colors and theme

--- app/src/main/java/com/kraigdev/myapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/kraigdev/myapplication/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DodgeBlue,
+    secondary = PurpleSecondary,
+    tertiary = TertiaryBlue
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = DodgeBlue,
+    secondary = PurpleSecondary,
+    tertiary = TertiaryBlue
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
